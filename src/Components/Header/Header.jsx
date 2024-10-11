cf21d80212b4c18df60f3b123a8e98502853cfb7
Hailu401 <123373063+Hailu401@users.noreply.github.com>
2024-10-11 16:27:44 -0400

    header position amend

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -12,69 +12,71 @@ function Header() {
  
   return (
     <>
-      <section>
-        <div className={classes.header_container}>
-          {/* { logo search} */}
-          <div className={classes.logo_container}>
-            <Link to="/">
-              <img
-                src="https://pngimg.com/uploads/amazon/small/amazon_PNG11.png"
-                alt="amazon logo"
-              />
-            </Link>
-            {/* delivery */}
-            <div className={classes.delivery}>
-              <span>
-                <SlLocationPin />
-              </span>
-              <div>
-                <p>Delivered to</p>
-                <span>Ethiopia</span>
+      <section className={classes.fixed}>
+        <section>
+          <div className={classes.header_container}>
+            {/* { logo search} */}
+            <div className={classes.logo_container}>
+              <Link to="/">
+                <img
+                  src="https://pngimg.com/uploads/amazon/small/amazon_PNG11.png"
+                  alt="amazon logo"
+                />
+              </Link>
+              {/* delivery */}
+              <div className={classes.delivery}>
+                <span>
+                  <SlLocationPin />
+                </span>
+                <div>
+                  <p>Delivered to</p>
+                  <span>Ethiopia</span>
+                </div>
               </div>
             </div>
-          </div>
 
-          {/* search */}
-          <div className={classes.search}>
-            <select name="" id="">
-              <option value="">All</option>
-            </select>
-            <input type="text" name="" id="" placeholder="search product" />
-            <BsSearch size={25} />
-          </div>
-
-          {/* other section */}
-          <div className={classes.order_container}>
-            <Link to="" className={classes.language}>
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg/255px-Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg.png"
-                alt="flag"
-              />
-              <select>
-                <option value="">EN</option>
+            {/* search */}
+            <div className={classes.search}>
+              <select name="" id="">
+                <option value="">All</option>
               </select>
-            </Link>
-            {/* three components */}
-            <Link to="/auth">
-              <div>
-                <p>Sign in</p>
-                <span>Account & Lists</span>
-              </div>
-            </Link>
-            {/* orders */}
-            <Link to="/orders">
-              <p>Returns</p>
-              <span>& Orders</span>
-            </Link>
-            {/* cart */}
-            <Link to="/cart" className={classes.cart}>
-              <BiCart size={35} />
-              <span>{basket.length}</span>
-            </Link>
+              <input type="text" name="" id="" placeholder="search product" />
+              <BsSearch size={25} />
+            </div>
+
+            {/* other section */}
+            <div className={classes.order_container}>
+              <Link to="" className={classes.language}>
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg/255px-Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg.png"
+                  alt="flag"
+                />
+                <select>
+                  <option value="">EN</option>
+                </select>
+              </Link>
+              {/* three components */}
+              <Link to="/auth">
+                <div>
+                  <p>Sign in</p>
+                  <span>Account & Lists</span>
+                </div>
+              </Link>
+              {/* orders */}
+              <Link to="/orders">
+                <p>Returns</p>
+                <span>& Orders</span>
+              </Link>
+              {/* cart */}
+              <Link to="/cart" className={classes.cart}>
+                <BiCart size={35} />
+                <span>{basket.length}</span>
+              </Link>
+            </div>
           </div>
-        </div>
+        </section>
+        <LowerHeader />
       </section>
-      <LowerHeader />
     </>
   );
 }
